3928ad97a69b111e022b9709ec9210abf14a8c33
bgMarkevych <chudik143@gmail.com>
2025-12-01 21:12:22 +0200

    BTSI-1871 Paywall. Hero video is not displaying on device

--- BotsiUI/Rendering/Components/BotsiHeroImageView.swift
+++ BotsiUI/Rendering/Components/BotsiHeroImageView.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import AVFoundation
+import _AVKit_SwiftUI
+import Combine
 
 @available(iOS 15.0, *)
 public struct BotsiHeroImageView: View {
@@ -18,7 +21,7 @@ public struct BotsiHeroImageView: View {
             ZStack {
                 Color.clear
                     .background(
-                        BotsiHeroImage(imageURL: model.backgroundImage)
+                        BotsiHeroImage(imageURL: model.backgroundImage, type: model.type)
                             .ignoresSafeArea()
                     )
                 Color.clear
@@ -26,9 +29,9 @@ public struct BotsiHeroImageView: View {
             }
 
         case .overlay:
-            BotsiHeroImage(imageURL: model.backgroundImage)
+            BotsiHeroImage(imageURL: model.backgroundImage, type: model.type)
         case .flat:
-            BotsiHeroImage(imageURL: model.backgroundImage)
+            BotsiHeroImage(imageURL: model.backgroundImage, type: model.type)
         }
     }
 }
@@ -37,37 +40,78 @@ public struct BotsiHeroImageView: View {
 private struct BotsiHeroImage: View {
     
     let imageURL: String?
+    let type: String?
     @Environment(\.imageLoadingTracker) private var imageLoadingTracker
+    @State private var player = AVPlayer()
+    @State private var cancellables = Set<AnyCancellable>()
 
     var body: some View {
         if let imageURL, let url = URL(string: imageURL) {
-            AsyncImage(url: url, transaction: Transaction(animation: .none)) { phase in
-                switch phase {
-                case .success(let image):
-                    image
-                        .resizable()
-                        .scaledToFill()
-                        .onAppear {
-                            imageLoadingTracker.onImageComplete(imageURL)
-                        }
-                case .failure(_):
-                    Color.gray.opacity(0.2)
-                        .onAppear {
-                            imageLoadingTracker.onImageComplete(imageURL)
-                        }
-                case .empty:
-                    Color.gray.opacity(0.2)
+            if type == "video" {
+                VideoPlayer(player: player)
+                    .edgesIgnoringSafeArea(.all)
+                    .navigationBarBackButtonHidden()
+                    .scaledToFill()
                     .onAppear {
-                        imageLoadingTracker.onImageStart(imageURL)
+                        let url = URL(string: imageURL)!
+                        player = AVPlayer(url: url)
+                        
+                        NotificationCenter.default.publisher(for: .AVPlayerItemDidPlayToEndTime, object: player.currentItem)
+                                .sink { _ in
+                                    player.seek(to: .zero)
+                                    player.play()
+                                }
+                                .store(in: &cancellables)
+                        
+                        player.currentItem?.publisher(for: \.status)
+                                .sink { status in
+                                    switch status {
+                                    case .readyToPlay:
+                                        imageLoadingTracker.onImageComplete(imageURL)
+                                    case .failed:
+                                        print("Video loading failed")
+                                    case .unknown:
+                                        imageLoadingTracker.onImageStart(imageURL)
+                                    @unknown default:
+                                        break
+                                    }
+                                }
+                                .store(in: &cancellables)
+                        player.play()
                     }
-                @unknown default:
-                    Color.gray.opacity(0.2)
-                        .onAppear {
-                            imageLoadingTracker.onImageComplete(imageURL)
-                        }
+                    .onDisappear {
+                        player.pause()
+                        cancellables.removeAll()
+                    }
+            } else {
+                AsyncImage(url: url, transaction: Transaction(animation: .none)) { phase in
+                    switch phase {
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .onAppear {
+                                imageLoadingTracker.onImageComplete(imageURL)
+                            }
+                    case .failure(_):
+                        Color.gray.opacity(0.2)
+                            .onAppear {
+                                imageLoadingTracker.onImageComplete(imageURL)
+                            }
+                    case .empty:
+                        Color.gray.opacity(0.2)
+                            .onAppear {
+                                imageLoadingTracker.onImageStart(imageURL)
+                            }
+                    @unknown default:
+                        Color.gray.opacity(0.2)
+                            .onAppear {
+                                imageLoadingTracker.onImageComplete(imageURL)
+                            }
+                    }
                 }
+                .drawingGroup()
             }
-            .drawingGroup()
         } else {
             Color.gray.opacity(0.2)
         }
